f10188809b8b236143c319fc7136ebdb1759ceff
mariaesantos238-png <maria.e.santos238@aluno.senai.br>
2026-09-04 11:35:21 -0300

    Create Cliente Controller

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace BancoSENAIAPI.Models
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get; set; }
+        public string NomeCliente { get; set; }
+        public string Cpf { get; set; }
+        public int NumeroAgencia { get; set; }
+        public decimal Saldototal { get; set; }
+        public string Sexo { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
