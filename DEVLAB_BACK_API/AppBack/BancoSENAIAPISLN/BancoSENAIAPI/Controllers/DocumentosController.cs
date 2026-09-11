97ee979baa4b7623ca1f1c21868ece77f040b971
mariaesantos238-png <maria.e.santos238@aluno.senai.br>
2026-09-11 08:37:24 -0300

    feat(documentos): simulacao do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -6,6 +6,14 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentosController : Controller
     {
-        
+        private readonly string _caminhoRaiz = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "ClienteArquivos"
+            );
+
+        private static List<Models.DocumentoMetadado>
+        _documentoMetadados= new List<Models.DocumentoMetadado>();
+
+        private static int _nextId = 1;
     }
 }
