dbaefd036cf5f5101f140431f9d9d1cc4931ccc8
mariaesantos238-png <maria.e.santos238@aluno.senai.br>
2026-09-11 09:01:03 -0300

    feat(documentos): criacao diretorio upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -23,6 +23,14 @@ namespace BancoSENAIAPI.Controllers
             {
                 return BadRequest("Nenhum arquivo foi enviado.");
             }
+
+            string pastaCliente = Path.Combine(_caminhoRaiz, codigoCliente.ToString());
+
+            if (Directory.Exists(pastaCliente))
+            {
+                Directory.CreateDirectory(pastaCliente);
+            }
+
         }
     }
 }
