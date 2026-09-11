f378d27bb77fa35383f4b06a82c65e37bcf319b8
mariaesantos238-png <maria.e.santos238@aluno.senai.br>
2026-09-11 09:08:17 -0300

    feat(documentos): definicao metadados no upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace BancoSENAIAPI.Controllers
 {
@@ -31,6 +32,11 @@ namespace BancoSENAIAPI.Controllers
                 Directory.CreateDirectory(pastaCliente);
             }
 
+            string extensao = Path.GetExtension(arquivo.FileName);
+
+            string nomeOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
+            string novoNome = $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
+            string caminhoFinal = Path.Combine(pastaCliente, novoNome);
         }
     }
 }
